ebb7ee26a294be1122fcd664253f6eab08dbc543
SpacePistache <lukeholley@hotmail.com>
2026-08-05 12:45:11 +0200

    mongo db task 1

---
+++ utils/db.mjs
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient(`mongodb://${host}:${port}`, {
+      useUnifiedTopology: true,
+    });
+
+    this.db = null;
+
+    this.client.connect((err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        this.db = this.client.db(database);
+      }
+    });
+  }
+
+  isAlive() {
+    return this.db !== null;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
