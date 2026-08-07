fc23d7f7596f81825a3cdf3deeb1ee46b414d43a
SpacePistache <lukeholley@hotmail.com>
2026-08-07 09:29:10 +0200

    task 5

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -7,3 +7,29 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 const { ObjectId } = mongodb;
+
+class FilesController {
+  static async postUpload(req, res) {
+  const token = req.header('X-Token');
+
+  const userId = await redisClient.get(`auth_${token}`);
+
+  if (!userId) {
+    return res.status(401).json({
+      error: 'Unauthorized',
+    });
+  }
+
+  const user = await dbClient.db.collection('users').findOne({
+    _id: new ObjectId(userId),
+  });
+
+  if (!user) {
+    return res.status(401).json({
+      error: 'Unauthorized',
+    });
+  }
+}
+}
+
+export default FilesController;
